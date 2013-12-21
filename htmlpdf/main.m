b885a3b55dc511776f9cc4c7fbd1c43f9e17c996
Wei <w31@users.noreply.github.com>
2013-12-21 08:20:06 -0600

    Initial commit.

---
+++ htmlpdf/main.m
@@ -0,0 +1,21 @@
+//
+//  main.m
+//  htmlpdf
+//
+//  Created by Wei on 12/21/13.
+//  Copyright (c) 2013 Wei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#define APP_NAME "htmlpdf"
+#define APP_VERSION "0.1"
+
+int main(int argc, const char * argv[])
+{
+    @autoreleasepool {
+        printf("%s %s\n", APP_NAME, APP_VERSION);
+    }
+    return 0;
+}
+
