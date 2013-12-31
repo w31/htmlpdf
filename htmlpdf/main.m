5ece92d221ea56274fff0404437ccb8c25691e1d
Wei <w31@users.noreply.github.com>
2013-12-31 00:27:13 -0600

    First-pass at putting everything together.

--- htmlpdf/main.m
+++ htmlpdf/main.m
@@ -7,6 +7,8 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <WebKit/WebKit.h>
+#import "WebViewDelegate.h"
 
 #define APP_NAME "htmlpdf"
 #define APP_VERSION "0.1"
@@ -15,6 +17,54 @@ int main(int argc, const char * argv[])
 {
     @autoreleasepool {
         printf("%s %s\n", APP_NAME, APP_VERSION);
+
+        [NSApplication sharedApplication];
+        
+        
+        Options *options = [[Options alloc]initWithArgc:argc argv:argv];
+        
+        if ([options hasValidOptions] == NO) {
+            printf("Usage: %s -i input -o output\n", APP_NAME);
+            return 1;
+        }
+        
+        
+        WebView *webView = [[WebView alloc] initWithFrame:NSMakeRect(0, 0, 1, 1)
+                                                frameName:[NSString stringWithFormat:@"%sFrame", APP_NAME]
+                                                groupName:[NSString stringWithFormat:@"%sGroup", APP_NAME]];
+        
+        NSWindow *window = [[NSWindow alloc] initWithContentRect:NSMakeRect(0, 0, 1, 1)
+                                                       styleMask:NSBorderlessWindowMask
+                                                         backing:NSBackingStoreNonretained defer:NO];
+        [window setContentView:webView];
+        
+        
+        WebPreferences *prefs = [WebPreferences standardPreferences];
+        
+        [prefs setLoadsImagesAutomatically:YES];
+        [prefs setAllowsAnimatedImages:YES];
+        [prefs setAllowsAnimatedImageLooping:NO];
+        [prefs setJavaEnabled:NO];
+        [prefs setPlugInsEnabled:NO];
+        [prefs setJavaScriptEnabled:YES];
+        [prefs setJavaScriptCanOpenWindowsAutomatically:NO];
+        prefs.shouldPrintBackgrounds = NO;
+        prefs.userStyleSheetEnabled = YES;
+        
+        
+        WebViewDelegate *webViewDelegate = [[WebViewDelegate alloc] initWithOptions:options];
+        [webView setFrameLoadDelegate:webViewDelegate];
+        [webView setApplicationNameForUserAgent:[NSString stringWithFormat:@"%s/%s", APP_NAME, APP_VERSION]];
+        [webView setPreferences:prefs];
+        [webView setMaintainsBackForwardList:NO];
+        [webView setMediaStyle:@"print"];
+        
+        NSURL *url = [NSURL fileURLWithPath:options.url];
+        NSURLRequest *urlRequest = [NSURLRequest requestWithURL:url];
+        [[webView mainFrame] loadRequest:urlRequest];
+        
+        
+        [[NSRunLoop currentRunLoop] run];
     }
     return 0;
 }
